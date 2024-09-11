b02369d2e28f99e9e49bbef8ed0fa3c7fdfec81b
Timo Fuite <T.Fuite@outlook.com>
2024-09-11 22:04:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+df = pd.read_csv('CSV_HERE')
+
+value_counts_dict = {}
+
+for column in df.columns:
+    if df[column].dtype == 'object':
+        value_counts = df[column].value_counts()
+        value_counts_dict[column] = value_counts
+
+for column, value_counts in value_counts_dict.items():
+    print(f"Value counts for column '{column}':")
+    print(value_counts)
+    print()
